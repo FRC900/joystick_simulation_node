b5ea6c7a00c10fa28ebdd87dbcf782f6d42854a3
Michael Todd <eltodd@gmail.com>
2023-02-05 19:41:35 +0000

    Remove nuisance prints

--- src/joystick_simulation_node/joystick.py
+++ src/joystick_simulation_node/joystick.py
@@ -44,7 +44,6 @@ class Joystick(QWidget):
         if not self.grabCenter:
             return [0,0]
         normVector = QLineF(self._center(), self.movingOffset)
-        print(normVector)
         result = []
         result.append(((normVector.x2() - normVector.x1()) / 50))
         if normVector.x2() < 1 :
@@ -52,7 +51,6 @@ class Joystick(QWidget):
         result.append((((normVector.y2() - normVector.y1()) / 50)))
         if normVector.y2() < 1 :
             result[1] = 0
-        print(result)
         return result
         currentDistance = normVector.length()
         angle = normVector.angle()
